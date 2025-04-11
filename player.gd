70ae0573f94d07ebf190b5dc65f3a69460c62cc3
Feavr555 <71680896+Feavr555@users.noreply.github.com>
2025-04-11 17:40:25 -0300

    Add files via upload

---
+++ player.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+@export var speed : int = 400
+var screen_size : Vector2
+
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+	pass
+
+
+func _process(delta: float) -> void:
+	var velocity : Vector2 = Vector2.ZERO
+	if Input.is_action_pressed("move_up"):
+		velocity += Vector2(0,-1)
+	if Input.is_action_pressed("move_down"):
+		velocity += Vector2(0,1)
+	if Input.is_action_pressed("move_right"):
+		velocity += Vector2(1,0)
+	if Input.is_action_pressed("move_left"):
+		velocity += Vector2(-1,0)
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO,screen_size)
+	pass
