8129ee2a7fe0ffbca9487582e2e01dd8c7847a5f
Feavr555 <71680896+Feavr555@users.noreply.github.com>
2025-04-13 14:32:23 -0300

    Add files via upload

--- background/background.gd
+++ background/background.gd
@@ -1,1 +1,11 @@
+extends Area2D
+@export var image : Image
 
+func loadData():
+	var data : Dictionary = {
+		"Position": position,
+		"Image": image
+	}
+	SystemSave.loadData(data)
+	SystemSave.saveGame()
+	pass
